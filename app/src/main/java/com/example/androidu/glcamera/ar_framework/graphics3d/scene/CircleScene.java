a67ef0b96066de54e1bbf4b3b33535a098c1daa6
Wilbert Veit <wilbertveit@rocketmail.com>
2018-05-03 23:18:43 -0700

    Updated CircleScene - CircleScene works now!

--- app/src/main/java/com/example/androidu/glcamera/ar_framework/graphics3d/scene/CircleScene.java
+++ app/src/main/java/com/example/androidu/glcamera/ar_framework/graphics3d/scene/CircleScene.java
@@ -3,13 +3,15 @@ package com.example.androidu.glcamera.ar_framework.graphics3d.scene;
 
 import com.example.androidu.glcamera.ar_framework.graphics3d.entity.Entity;
 import com.example.androidu.glcamera.ar_framework.util.GeoMath;
-import com.example.androidu.glcamera.ar_framework.util.VectorMath;
+
 
 public class CircleScene extends Scene{
-    // TODO - create CircleScene code
+    private static final String TAG = "waka-circleScene";
 
     private float mRadius = 10;
     private float[] mCenter = new float[3];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void setRadius(float radius){
         mRadius = radius;
@@ -19,24 +21,23 @@ public class CircleScene extends Scene{
         GeoMath.latLonAltToXYZ(latLonAlt, mCenter);
     }
 
-    @Override
-    public void draw(float[] projectionMatrix, float[] viewMatrix){
 
-        Entity entity = new Entity();
-        float[] north = {0, 0, -1};
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-        for(Entity e : mEntityList){
-            // Copy the entity's matrix and place in new entity
-            entity.setModelMatrix(e.getModelMatrix());
-
-            // Draw entity with model matrix from new entity
-            float[] position = e.getPosition();
-            float[] diffVector = new float[]{position[0] - mCenter[0], 0, position[2] - mCenter[2]};
-            VectorMath.normalizeInPlace(diffVector); diffVector[0] *= mRadius; diffVector[2] *= mRadius;
-            entity.setPosition(diffVector[0], diffVector[1], diffVector[2]);
-
-            e.draw(projectionMatrix, viewMatrix, entity.getModelMatrix());
+    public void update(){
+        for(int i = 0; i < mEntityList.size(); i++){
+            Entity entity = mEntityList.get(i);
+            updateEntity(entity);
         }
+    }
+
+    private void updateEntity(Entity entity){
+        float[] pos = entity.getPosition();
+        float angle = GeoMath.xyzBearing(mCenter, pos);
+        float distance = GeoMath.xyzDistance(mCenter, pos);
+        float scale = distance / mRadius;
+        entity.setYaw(-angle);
+        entity.setScale(scale, scale, scale);
     }
 
 }
