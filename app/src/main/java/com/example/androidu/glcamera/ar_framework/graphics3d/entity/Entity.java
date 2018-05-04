c8210838ee6083dc527638271b88670e3e9122e3
Wilbert Veit <wilbertveit@rocketmail.com>
2018-05-03 22:14:20 -0700

    Update Entity: Fixed calculations and simplified interface

--- app/src/main/java/com/example/androidu/glcamera/ar_framework/graphics3d/entity/Entity.java
+++ app/src/main/java/com/example/androidu/glcamera/ar_framework/graphics3d/entity/Entity.java
@@ -1,8 +1,6 @@
 package com.example.androidu.glcamera.ar_framework.graphics3d.entity;
 
-import android.nfc.Tag;
 import android.opengl.Matrix;
-import android.util.Log;
 
 import com.example.androidu.glcamera.ar_framework.graphics3d.drawable.Drawable;
 import com.example.androidu.glcamera.ar_framework.util.GeoMath;
@@ -10,134 +8,163 @@ import com.example.androidu.glcamera.ar_framework.util.MatrixMath;
 import com.example.androidu.glcamera.ar_framework.util.VectorMath;
 
 
-public class Entity extends Drawable{
+public class Entity extends Drawable {
     private static final String TAG = "waka-Entity";
 
-    private float scaleX = 1, scaleY = 1, scaleZ = 1;
-    private float posX, posY, posZ;
+    private float[] scale = {1, 1, 1};
+    private float[] pos = new float[3];
+    private float[] latLonAlt = new float[3];
     private float yawAngle;
     private float[] modelMatrix = new float[16];
-    private Drawable drawable = null;
 
-    private float[] mColor = null;
+    private Drawable drawable = null;
+    private float[] color = null;
 
     private boolean matrixIsClean = false;
+    private boolean latLonAltIsClean = false;
 
-    public void reset(){
-        scaleX = 1; scaleY = 1; scaleZ = 1;
-        posX = 0; posY = 0; posZ = 0;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void reset() {
+        scale[0] = scale[1] = scale[2] = 1;
+        pos[0] = pos[1] = pos[2] = 0;
         yawAngle = 0;
         matrixIsClean = false;
+        latLonAltIsClean = false;
     }
 
-    public void setScale(float scaleX, float scaleY, float scaleZ){
-        this.scaleX = scaleX;
-        this.scaleY = scaleY;
-        this.scaleZ = scaleZ;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setScale(float x, float y, float z) {
+        this.scale[0] = x;
+        this.scale[1] = y;
+        this.scale[2] = z;
         matrixIsClean = false;
     }
 
-    public void setPosition(float x, float y, float z){
-        posX = x; posY = y; posZ = z;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setPosition(float x, float y, float z) {
+        pos[0] = x;
+        pos[1] = y;
+        pos[2] = z;
+        matrixIsClean = false;
+        latLonAltIsClean = false;
+    }
+
+    public void move(float dx, float dy, float dz) {
+        pos[0] += dx;
+        pos[1] += dy;
+        pos[2] += dz;
+        matrixIsClean = false;
+        latLonAltIsClean = false;
+    }
+
+    public void slide(float dx, float dy, float dz) {
+        pos[0] += dx * Math.cos(VectorMath.degreesToRad(yawAngle)) + dz * Math.sin(VectorMath.degreesToRad(yawAngle));
+        pos[1] += dy;
+        pos[2] += dz * Math.cos(VectorMath.degreesToRad(yawAngle)) - dx * Math.sin(VectorMath.degreesToRad(yawAngle));
+        matrixIsClean = false;
+        latLonAltIsClean = false;
+    }
+
+    public float[] getPosition() {
+        return pos;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setLatLonAlt(float[] latLonAlt) {
+        VectorMath.copyVec(latLonAlt, this.latLonAlt, 3);
+        latLonAltIsClean = true;
+        GeoMath.latLonAltToXYZ(latLonAlt, pos);
         matrixIsClean = false;
     }
 
-    public void move(float dx, float dy, float dz){
-        posX += dx; posY += dy; posZ += dz;
-        matrixIsClean = false;
+    public float[] getLatLonAlt() {
+        if (!latLonAltIsClean) {
+            GeoMath.xyzToLatLonAlt(pos, latLonAlt);
+            latLonAltIsClean = true;
+        }
+
+        return latLonAlt;
     }
 
-    public void slide(float dx, float dy, float dz){
-        posX += dx * Math.cos(VectorMath.degreesToRad(yawAngle)) + dz * Math.sin(VectorMath.degreesToRad(yawAngle));
-        posY += dy;
-        posZ += dz * Math.cos(VectorMath.degreesToRad(yawAngle)) - dx * Math.sin(VectorMath.degreesToRad(yawAngle));
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public void setPositionLatLonAlt(float[] latLonAlt){
-        float[] xyz = new float[3];
-        GeoMath.latLonAltToXYZ(latLonAlt, xyz);
-        setPosition(xyz[0], xyz[1], xyz[2]);
-        matrixIsClean = false;
-    }
-
-
-    public void setYaw(float angle){
+    public void setYaw(float angle) {
         yawAngle = angle;
         matrixIsClean = false;
     }
 
-    public void yaw(float dAngle){
+    public void yaw(float dAngle) {
         yawAngle += dAngle;
         matrixIsClean = false;
     }
 
-    private void updateModelMatrix(){
-        Matrix.translateM(modelMatrix, 0, MatrixMath.IDENTITY_MATRIX, 0, posX, posY, posZ);
-        Matrix.rotateM(modelMatrix, 0, yawAngle, 0, 1, 0);
-        Matrix.scaleM(modelMatrix, 0, scaleX, scaleY, scaleZ);
-
-        matrixIsClean = true;
+    public float getYaw() {
+        return yawAngle;
     }
 
-    // TODO Entity: add setLookAt(...) method
-//    public void setLookAt(float[] eye, float[] look, float[] up){
-//        Matrix.setLookAtM(modelMatrix, 0, eye[0], eye[1], eye[2], look[0], look[1], look[2], up[0], up[1], up[2]);
-//        Matrix.
-//        matrixIsClean = true;
-//    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // TODO Entity: add setModelMatrix method
-    public void setModelMatrix(float[] newMatrix){}
-
-    // TODO Entity: Add the movement methods that camera has
-
-    // TODO Entity: Add getters for position, rotation and other things
-    public float[] getPosition(){return new float[]{0, 0, 0};}
-
-    public float[] getModelMatrix(){
-        if(!matrixIsClean)
+    public float[] getModelMatrix() {
+        if (!matrixIsClean)
             updateModelMatrix();
 
         return modelMatrix;
     }
 
-    public void setDrawable(Drawable d){
+    private void updateModelMatrix() {
+        Matrix.translateM(modelMatrix, 0, MatrixMath.IDENTITY_MATRIX, 0, pos[0], pos[1], pos[2]);
+        Matrix.rotateM(modelMatrix, 0, yawAngle, 0, 1, 0);
+        Matrix.scaleM(modelMatrix, 0, scale[0], scale[1], scale[2]);
+
+        matrixIsClean = true;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setDrawable(Drawable d) {
         drawable = d;
     }
 
     @Override
-    public void draw(float[] projection, float[] view, float[] model){
-        if(drawable == null)
+    public void setColor(float[] color) {
+        if (color == null || color.length != 4) {
+            this.color = null;
+            return;
+        }
+
+        if (this.color == null)
+            this.color = new float[4];
+
+        VectorMath.copyVec(color, this.color, 4);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void draw(float[] projection, float[] view, float[] model) {
+        if (drawable == null)
             return;
 
-        if(mColor != null){
-            drawable.drawColor(projection, view, model, mColor);
+        if (color != null) {
+            drawable.drawColor(projection, view, model, color);
         }
 
         drawable.draw(projection, view, model);
     }
 
     @Override
-    public void draw(float[] matrix){
-        if(drawable == null)
+    public void draw(float[] matrix) {
+        if (drawable == null)
             return;
 
-        if(mColor != null)
-            drawable.drawColor(matrix, mColor);
+        if (color != null)
+            drawable.drawColor(matrix, color);
 
         drawable.draw(matrix);
     }
 
-    @Override
-    public void setColor(float[] color){
-        if(color == null || color.length != 4){
-            mColor = null;
-            return;
-        }
-
-        if(mColor == null)
-            mColor = new float[4];
-
-        VectorMath.copyVec(color, mColor, 4);
-    }
 }
